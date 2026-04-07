fd582c5c2cb1ed7fdc9619672eab16a523b2e538
Makendra <gamerchan756@gmail.com>
2026-04-07 10:59:43 -0400

    Initial Commit

---
+++ Group2Proposal2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Group2Proposal2
+//
+//  Created by Makendra Phipps on 4/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
